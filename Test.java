9f3e582c8a6e301b2d2f6c639a423fe3099c7f3b
OzsvathHunor <45239731+OzsvathHunor@users.noreply.github.com>
2023-01-02 10:06:27 +0100

    Minimum metodus letrehozasa

--- Test.java
+++ Test.java
@@ -1,6 +1,14 @@
-import java.util.Random;
-
 public class Test {
+
+    public int minimum(int[] tomb){
+        int min = Integer.MAX_VALUE;
+        for (var i:tomb) {
+            if (i < min){
+                min = i;
+            }
+        }
+        return min;
+    }
     public static void main(String[] args){
         int[] tomb = new int[20];
 
